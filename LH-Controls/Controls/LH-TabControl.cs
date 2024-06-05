d2785e977b1da878df9d35697d44a35ed63a932f
李航 <@LAPTOP>
2024-06-05 23:23:41 +0800

    添加项目文件。

---
+++ LH-Controls/Controls/LH-TabControl.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace LH_Controls.Controls
+{
+    public class LH_TabControl:TabControl
+    {
+        public LH_TabControl()
+        {
+            this.SetStyle(ControlStyles.UserPaint,true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint,true);
+            this.SetStyle(ControlStyles.Selectable,true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer,true);
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.DrawMode = TabDrawMode.OwnerDrawFixed;
+            this.DrawItem += LH_TabControl_DrawItem;
+            this.SizeMode = TabSizeMode.Fixed;
+        }
+        private void LH_TabControl_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Rectangle rect =this.GetTabRect(e.Index);
+            graphics.DrawRectangle(Pens.Black, rect);
+            if (e.Index == this.SelectedIndex)
+            {
+                graphics.FillRectangle(new SolidBrush(Color.Green), rect);
+                graphics.DrawRectangle(Pens.Black, rect);
+                StringFormat stringFormat = new StringFormat();
+                stringFormat.LineAlignment = StringAlignment.Center;
+                stringFormat.Alignment = StringAlignment.Center;
+                graphics.DrawString(TabPages[e.Index].Text, this.Font, new SolidBrush(Color.White), new PointF(rect.X + 3, rect.Y + 6), stringFormat);
+            }
+            else
+            {
+                graphics.FillRectangle(new SolidBrush(Color.Blue), rect);
+                StringFormat stringFormat = new StringFormat();
+                stringFormat.Alignment = StringAlignment.Center;
+                stringFormat.LineAlignment = StringAlignment.Center;
+                graphics.DrawString(TabPages[e.Index].Text, this.Font, new SolidBrush(Color.Red), new PointF(rect.X + 3, rect.Y + 6), stringFormat);
+
+            }
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            for (int i = 0; i < TabPages.Count; i++)
+            {
+                Rectangle rect = this.GetTabRect(i);
+                if (i == 0)
+                {
+                    rect.Inflate(-2, 2);
+                }
+                else
+                {
+                    rect.Inflate(2, 2);
+                }
+                if (i == this.SelectedIndex)
+                {
+                    graphics.FillRectangle(new SolidBrush(Color.Green), rect);
+                    StringFormat stringFormat = new StringFormat();
+                    stringFormat.LineAlignment = StringAlignment.Center;
+                    stringFormat.Alignment = StringAlignment.Center;
+                    graphics.DrawString(TabPages[i].Text, new Font("宋体",16,FontStyle.Bold), new SolidBrush(Color.White), rect, stringFormat);
+                }
+                else if (i == TabPages.Count - 1)
+                {
+                    graphics.FillRectangle(new SolidBrush(Color.Blue), rect.X,rect.Y,this.Width - rect.X,rect.Height);
+                    StringFormat stringFormat = new StringFormat();
+                    stringFormat.Alignment = StringAlignment.Center;
+                    stringFormat.LineAlignment = StringAlignment.Center;
+                    graphics.DrawString(TabPages[i].Text, this.Font, new SolidBrush(Color.Red), rect, stringFormat);
+                }
+                else
+                {
+                    graphics.FillRectangle(new SolidBrush(Color.Blue), rect); 
+                    StringFormat stringFormat = new StringFormat();
+                    stringFormat.Alignment = StringAlignment.Center;
+                    stringFormat.LineAlignment = StringAlignment.Center;
+                    graphics.DrawString(TabPages[i].Text, this.Font, new SolidBrush(Color.Red), rect, stringFormat);
+                }
+            }
+            //graphics.FillRectangle();
+            base.OnPaint(e);
+        }
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
+            Graphics graphics = pevent.Graphics;
+            graphics.FillRectangle(new SolidBrush(Color.Blue),this.ClientRectangle);
+            base.OnPaintBackground(pevent);
+        }
+    }
+}
---
+++ LH-Controls/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace LH_Controls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lH_TabControl1 = new LH_Controls.Controls.LH_TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.lH_TabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lH_TabControl1
+            // 
+            this.lH_TabControl1.Controls.Add(this.tabPage1);
+            this.lH_TabControl1.Controls.Add(this.tabPage2);
+            this.lH_TabControl1.Controls.Add(this.tabPage3);
+            this.lH_TabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.lH_TabControl1.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lH_TabControl1.ItemSize = new System.Drawing.Size(200, 70);
+            this.lH_TabControl1.Location = new System.Drawing.Point(189, 45);
+            this.lH_TabControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.lH_TabControl1.Name = "lH_TabControl1";
+            this.lH_TabControl1.Padding = new System.Drawing.Point(0, 0);
+            this.lH_TabControl1.SelectedIndex = 0;
+            this.lH_TabControl1.Size = new System.Drawing.Size(918, 532);
+            this.lH_TabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.lH_TabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Black;
+            this.tabPage1.Location = new System.Drawing.Point(4, 74);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(910, 454);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "测试1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 74);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(910, 454);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "测试2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 74);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(910, 454);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "压力测试";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1278, 763);
+            this.Controls.Add(this.lH_TabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.lH_TabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.LH_TabControl lH_TabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
