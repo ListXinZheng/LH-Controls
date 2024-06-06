95cf8fb50d349f2516a42181b592c578d46c4716
李航 <@LAPTOP>
2024-06-06 23:23:29 +0800

    初步修改完成tabcontrol的基本属性重绘

--- LH-Controls/Controls/LH-TabControl.cs
+++ LH-Controls/Controls/LH-TabControl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,60 @@ namespace LH_Controls.Controls
 {
     public class LH_TabControl:TabControl
     {
+        private Color _unselectedtabforecolor = Color.Red;
+        [Browsable(true)]
+        [Category("LH")]
+        [Description("未选中标签的文本前景色")]
+        public Color UnSeclectedtabForeColor
+        {
+            get { return _unselectedtabforecolor; }
+            set { _unselectedtabforecolor = value;this.Invalidate(); }
+        }
+        private Color _selectedtabforecolor = Color.White;
+        [Browsable(true)]
+        [Category("LH")]
+        [Description("选中标签的文本前景色")]
+        public Color SelectedTabForeColor
+        {
+            get { return _selectedtabforecolor; }
+            set { _selectedtabforecolor = value; this.Invalidate(); }
+        }
+        private Color _unselecttabbackcolor = Color.Blue;
+        [Browsable(true)]
+        [Category("LH")]
+        [Description("未选中标签的文本背景色")]
+        public Color UnSelectTabBackColor
+        {
+            get { return _unselecttabbackcolor; }
+            set { _unselecttabbackcolor = value; this.Invalidate(); }
+        }
+        private Color _selecttabbackcolor = Color.Green;
+        [Browsable(true)]
+        [Category("LH")]
+        [Description("选中标签的文本背景色")]
+        public Color SelectTabBackColor
+        {
+            get { return _selecttabbackcolor; }
+            set { _selecttabbackcolor = value; this.Invalidate(); }
+        }
+        private Font _unselectfont = new Font("微软雅黑",12);
+        [Browsable(true)]
+        [Category("LH")]
+        [Description("未选中标签的字体")]
+        public Font UnSelectFont
+        {
+            get { return _unselectfont; }
+            set { _unselectfont = value; this.Invalidate(); }
+        }
+        private Font _selectfont = new Font("微软雅黑", 16);
+        [Browsable(true)]
+        [Category("LH")]
+        [Description("选中标签的字体")]
+        public Font SelectFont
+        {
+            get { return _selectfont; }
+            set { _selectfont = value; this.Invalidate(); }
+        }
         public LH_TabControl()
         {
             this.SetStyle(ControlStyles.UserPaint,true);
@@ -18,36 +73,21 @@ namespace LH_Controls.Controls
             this.SetStyle(ControlStyles.ResizeRedraw, true);
             this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
             this.DrawMode = TabDrawMode.OwnerDrawFixed;
-            this.DrawItem += LH_TabControl_DrawItem;
             this.SizeMode = TabSizeMode.Fixed;
+            this.Padding = new Point(0,0);
+            this.Margin = new Padding(0,0,0,0);
+            this.Font = new Font("微软雅黑", 12);
         }
-        private void LH_TabControl_DrawItem(object sender, DrawItemEventArgs e)
+        private void SetGraphicsStyle(Graphics g)
         {
-            Graphics graphics = e.Graphics;
-            Rectangle rect =this.GetTabRect(e.Index);
-            graphics.DrawRectangle(Pens.Black, rect);
-            if (e.Index == this.SelectedIndex)
-            {
-                graphics.FillRectangle(new SolidBrush(Color.Green), rect);
-                graphics.DrawRectangle(Pens.Black, rect);
-                StringFormat stringFormat = new StringFormat();
-                stringFormat.LineAlignment = StringAlignment.Center;
-                stringFormat.Alignment = StringAlignment.Center;
-                graphics.DrawString(TabPages[e.Index].Text, this.Font, new SolidBrush(Color.White), new PointF(rect.X + 3, rect.Y + 6), stringFormat);
-            }
-            else
-            {
-                graphics.FillRectangle(new SolidBrush(Color.Blue), rect);
-                StringFormat stringFormat = new StringFormat();
-                stringFormat.Alignment = StringAlignment.Center;
-                stringFormat.LineAlignment = StringAlignment.Center;
-                graphics.DrawString(TabPages[e.Index].Text, this.Font, new SolidBrush(Color.Red), new PointF(rect.X + 3, rect.Y + 6), stringFormat);
-
-            }
+            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
         }
         protected override void OnPaint(PaintEventArgs e)
         {
             Graphics graphics = e.Graphics;
+            SetGraphicsStyle(graphics);
             for (int i = 0; i < TabPages.Count; i++)
             {
                 Rectangle rect = this.GetTabRect(i);
@@ -61,37 +101,30 @@ namespace LH_Controls.Controls
                 }
                 if (i == this.SelectedIndex)
                 {
-                    graphics.FillRectangle(new SolidBrush(Color.Green), rect);
+                    graphics.FillRectangle(new SolidBrush(_selecttabbackcolor), rect);
                     StringFormat stringFormat = new StringFormat();
                     stringFormat.LineAlignment = StringAlignment.Center;
                     stringFormat.Alignment = StringAlignment.Center;
-                    graphics.DrawString(TabPages[i].Text, new Font("宋体",16,FontStyle.Bold), new SolidBrush(Color.White), rect, stringFormat);
+                    graphics.DrawString(TabPages[i].Text, _selectfont, new SolidBrush(_selectedtabforecolor), rect, stringFormat);
                 }
                 else if (i == TabPages.Count - 1)
                 {
-                    graphics.FillRectangle(new SolidBrush(Color.Blue), rect.X,rect.Y,this.Width - rect.X,rect.Height);
+                    graphics.FillRectangle(new SolidBrush(_unselecttabbackcolor), rect.X,rect.Y,this.Width - rect.X-4,rect.Height);
                     StringFormat stringFormat = new StringFormat();
                     stringFormat.Alignment = StringAlignment.Center;
                     stringFormat.LineAlignment = StringAlignment.Center;
-                    graphics.DrawString(TabPages[i].Text, this.Font, new SolidBrush(Color.Red), rect, stringFormat);
+                    graphics.DrawString(TabPages[i].Text, _unselectfont, new SolidBrush(_unselectedtabforecolor), rect, stringFormat);
                 }
                 else
                 {
-                    graphics.FillRectangle(new SolidBrush(Color.Blue), rect); 
+                    graphics.FillRectangle(new SolidBrush(_unselecttabbackcolor), rect); 
                     StringFormat stringFormat = new StringFormat();
                     stringFormat.Alignment = StringAlignment.Center;
                     stringFormat.LineAlignment = StringAlignment.Center;
-                    graphics.DrawString(TabPages[i].Text, this.Font, new SolidBrush(Color.Red), rect, stringFormat);
+                    graphics.DrawString(TabPages[i].Text, _unselectfont, new SolidBrush(_unselectedtabforecolor), rect, stringFormat);
                 }
             }
-            //graphics.FillRectangle();
             base.OnPaint(e);
-        }
-        protected override void OnPaintBackground(PaintEventArgs pevent)
-        {
-            Graphics graphics = pevent.Graphics;
-            graphics.FillRectangle(new SolidBrush(Color.Blue),this.ClientRectangle);
-            base.OnPaintBackground(pevent);
         }
     }
 }
--- LH-Controls/Form1.Designer.cs
+++ LH-Controls/Form1.Designer.cs
@@ -43,42 +43,49 @@
             this.lH_TabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
             this.lH_TabControl1.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lH_TabControl1.ItemSize = new System.Drawing.Size(200, 70);
-            this.lH_TabControl1.Location = new System.Drawing.Point(189, 45);
+            this.lH_TabControl1.Location = new System.Drawing.Point(189, 48);
             this.lH_TabControl1.Margin = new System.Windows.Forms.Padding(0);
             this.lH_TabControl1.Name = "lH_TabControl1";
             this.lH_TabControl1.Padding = new System.Drawing.Point(0, 0);
             this.lH_TabControl1.SelectedIndex = 0;
-            this.lH_TabControl1.Size = new System.Drawing.Size(918, 532);
+            this.lH_TabControl1.SelectedTabForeColor = System.Drawing.Color.Green;
+            this.lH_TabControl1.SelectFont = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lH_TabControl1.SelectTabBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lH_TabControl1.Size = new System.Drawing.Size(881, 529);
             this.lH_TabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.lH_TabControl1.TabIndex = 0;
+            this.lH_TabControl1.UnSeclectedtabForeColor = System.Drawing.Color.Red;
+            this.lH_TabControl1.UnSelectFont = new System.Drawing.Font("微软雅黑", 14F);
+            this.lH_TabControl1.UnSelectTabBackColor = System.Drawing.Color.MidnightBlue;
             // 
             // tabPage1
             // 
-            this.tabPage1.BackColor = System.Drawing.Color.Black;
+            this.tabPage1.BackColor = System.Drawing.Color.RosyBrown;
             this.tabPage1.Location = new System.Drawing.Point(4, 74);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(0);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Size = new System.Drawing.Size(910, 454);
+            this.tabPage1.Size = new System.Drawing.Size(873, 451);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "测试1";
             // 
             // tabPage2
             // 
+            this.tabPage2.BackColor = System.Drawing.Color.DimGray;
             this.tabPage2.Location = new System.Drawing.Point(4, 74);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(0);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(910, 454);
+            this.tabPage2.Size = new System.Drawing.Size(873, 451);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "测试2";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
+            this.tabPage3.BackColor = System.Drawing.Color.SeaGreen;
             this.tabPage3.Location = new System.Drawing.Point(4, 74);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(910, 454);
+            this.tabPage3.Size = new System.Drawing.Size(873, 451);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "压力测试";
-            this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
